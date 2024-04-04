cfc0cf342878654d1697d5bb8f7c27e2afec00b4
lijnati <lijnati3@gmail.com>
2024-04-04 16:43:25 +0300

    debugging:fizzbuzz.py 3nd time

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -3,36 +3,23 @@
 """
 import sys
 
-
 def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
-    if n < 1:
-        return
-
-    tmp_result = []
-    for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+    result = ""
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            result += "FizzBuzz "
+        elif i % 3 == 0:
+            result += "Fizz "
+        elif i % 5 == 0:
+            result += "Buzz "
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+            result += str(i) + " "
+    return result.strip()
 
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python 0-fizzbuzz.py <number>")
         sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    
+    n = int(sys.argv[1])
+    print(fizzbuzz(n))
